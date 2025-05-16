2bae83d95cdb31d4124499dd49a9efc45f611f05
Samrudh Raikote <samrudhraikote@Samrudhs-MacBook-Air.local>
2025-05-16 23:16:22 +0530

    Update Open Graph image to match favicon

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.samrudhraikote.me',
+  siteUrl: 'https://samrudhraikote.me',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ['/admin', '/draft'],
+  exclude: ['/admin', '/draft', '/blog/blog'],
 };
